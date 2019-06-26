b6bc846d39f7e964c93fccb5e4f44ed542271f11
aleksojem <aleksojem@gmail.com>
2019-06-27 00:36:41 +0300

    Mark misstakes in code

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {recipes} from "./tempList";
 import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
 
-calss App extends Component {
+class App extends Component {
   state = {
     recipes: recipes,
         url: "https://www.food2fork.com/api/search?key=0d352ac2d13aa49c465b8438363abedc"
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -3,12 +3,21 @@ import Recipe from "./Recipe";
 import RecipeSearch from "./RecipeSearch";
 export default class RecipeList extends Component {
     render() {
-        return (
-            <React.Fragment>
-            <h1>Hello From List</h1>
-            <RecipeSearch />
+    const {recipes} = this.props;
+    return (
+        <React.Fragment>
+         <RecipeSearch />
+<div className="container my-5">
+    {/* title */}
+    <div className="row">
+        <div className="col-10 mx-auto col-md-6 text-center text-uppercase mb-3">
+        <h1 className="text-slanted">recipe list</h1>
+        </div>
+    </div>
+    {/* end of title */}
+</div>
             <Recipe />
-            </React.Fragment>
-        )
+        </React.Fragment>
+        );
     }
 }
