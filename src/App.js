83ccf95b268f57aa48fd5b7955b85095d14c42ef
aleksojem <aleksojem@gmail.com>
2019-06-27 00:19:21 +0300

    Add json to app.js file

--- src/App.js
+++ src/App.js
@@ -3,15 +3,35 @@ import "./App.css";
 import {recipes} from "./tempList";
 import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
-calss App extends Comment {
-function App {
+
+calss App extends Component {
   state = {
-    recipes:[],
-    url: "https://www.food2fork.com/api/search?key=0d352ac2d13aa49c465b8438363abedc"
+    recipes: recipes,
+        url: "https://www.food2fork.com/api/search?key=0d352ac2d13aa49c465b8438363abedc"
   };
+
+//   async getRecipes() {
+//     try {
+//     const data = await fetch(this.state.url);
+//     const jsonData = await data.jason();
+//     this.setState({
+//       recipes: jsonData.recipes
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// componentDidMount() {
+//   this.getRecipes();
+//}
+
+render() {
+  //console.log(this.state.recipes);
+  
   return (
     <React.Fragment>
-      <RecipeList />
+      <RecipeList recipes={this.state.recipes} />
       <RecipeDetails />
     </React.Fragment>
       );
