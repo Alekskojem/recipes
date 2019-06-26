992903f0788bf9dcef87fce85a036ecafb34c1e1
aleksojem <aleksojem@gmail.com>
2019-06-26 23:49:03 +0300

    Add API to

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import "./App.css";
 import {recipes} from "./tempList";
 import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
-function App() {
+calss App extends Comment {
+function App {
+  state = {
+    recipes:[],
+    url: "https://www.food2fork.com/api/search?key=0d352ac2d13aa49c465b8438363abedc"
+  };
   return (
     <React.Fragment>
       <RecipeList />
       <RecipeDetails />
     </React.Fragment>
       );
+  }
 }
 
 export default App;
